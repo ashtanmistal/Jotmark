717440feeb41504574afc9f0a7d564631125a803
n1l8q <ashtanm1@students.cs.ubc.ca>
2023-01-04 18:57:28 -0800

    changed default build space limits; fixed default colors to sensible colors

--- src/app/service/settings.service.ts
+++ src/app/service/settings.service.ts
@@ -5,20 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class SettingsService {
   private _settings: any = {
-    editorFontSize: 8,
+    editorFontSize: 14,
     editorFontFamily: "monospace",
 
-    themePrimary: "#3f51b5",
-    themeAccent: "#ff4081",
-    themeTertiary: "#ff0000",
-    themeBackground: "#32b623", /* main background */
-    themeBackgroundAlt: "#18d5bf", /* a slightly darker background color for things like the editor */
-    themeText: "#ffa400", /* text color for the editor and primary text */
-    themeSecondaryText: "#5b2626", /* text color for secondary text (subtitles, etc) */
+    themePrimary: "#d7d7d7",
+    themeAccent: "#2f0101",
+    themeTertiary: "#a1a1a1",
+    themeBackground: "#ffffff", /* main background */
+    themeBackgroundAlt: "#f3f3f3", /* a slightly darker background color for things like the editor */
+    themeText: "#000000", /* text color for the editor and primary text */
+    themeSecondaryText: "#4f4f4f", /* text color for secondary text (subtitles, etc) */
 
-    previewFontSize: 8,
+    previewFontSize: 16,
     previewFontFamily: "Roboto",
-    defaultNoteColor: "#310d46",
+    defaultNoteColor: "#b7b7b7",
   };
 
   constructor() { }
